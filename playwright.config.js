1f9242eb030ab5b21ef9ad53d9c2c9cf6c56bf6f
subir-kundo-qa <kundosubir@gmail.com>
2025-11-23 23:21:33 +0600

    first commit

---
+++ playwright.config.js
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export default defineConfig({
+  testDir: './tests',
+  timeout: 40 * 1000, // Global timeout for each test (40s)
+  expect: { timeout: 40 * 1000 }, // Timeout for assertions (40s)
+  reporter: 'html',
+  use: {
+    browserName: 'chromium',
+    headless: false, // Recommended for CI or automation
+    screenshot: 'only-on-failure',
+    trace: 'on-first-retry',
+  },
+});
